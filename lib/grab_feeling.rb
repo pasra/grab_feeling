a510a098b807b1bf10b5df729faaa6f2312233b3
Shota Fukumori (sora_h) <sorah@tubusu.net>
2011-11-22 13:06:54 +0900

    add requires

--- lib/grab_feeling.rb
+++ lib/grab_feeling.rb
@@ -6,3 +6,11 @@ ENV["RACK_ENV"] ||= "development"
 require 'rubygems'
 require 'bundler'
 Bundler.setup
+
+require 'grab_feeling/db'
+require 'grab_feeling/model/dictionary'
+require 'grab_feeling/model/log'
+require 'grab_feeling/model/player'
+require 'grab_feeling/model/room'
+require 'grab_feeling/model/status'
+require 'grab_feeling/model/theme'
