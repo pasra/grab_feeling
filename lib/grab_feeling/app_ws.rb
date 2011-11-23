e15b9487338a889219674ec9789b0e65035acd4f
Shota Fukumori (sora_h) <sorah@tubusu.net>
2011-11-23 10:29:38 +0900

    socket pool

--- lib/grab_feeling/app_ws.rb
+++ lib/grab_feeling/app_ws.rb
@@ -6,7 +6,7 @@ require 'json'
 
 module GrabFeeling
   class SocketApp < Sinatra::Base
-    @@sockets = {}
+    @@sockets = SocketPool.new
     @@event_hooks = {}
     @@logger = Logger.new(STDOUT)
     @@websocket = ->{}
@@ -27,7 +27,18 @@ module GrabFeeling
       @@logger.info("Hello!")
     end
 
-    websocket do |s|
+    websocket do |socket|
+      socket.onopen do
+      end
+
+      socket.onmessage do
+      end
+
+      socket.onclose do
+      end
+
+      socket.onerror do
+      end
     end
 
     configure :development do
@@ -40,8 +51,7 @@ module GrabFeeling
       set :views => Proc.new { File.join(root, 'views') }
       set :default_locale, 'ja'
       ::I18n.load_path += Dir["#{root}/i18n/*.yml"]
-      use Rack::Session::Cookie,
-        :expire_after => 60 * 60 * 24 * 12
+      use Rack::Session::Cookie, :expire_after => 60*60*24*12
 
       ws_opt = if Config["websocket"] && Config["websocket"]["socket"]
                  {host: Config["websocket"]["socket"], port: nil}
@@ -60,6 +70,53 @@ module GrabFeeling
         @@logger.info("Event received: #{name}")
         (@@event_hooks[name] ||= []).each{|x| x[obj] }
       end
+    end
+  end
+
+  class SocketPool
+    def initialize
+      @pool = {}
+      @pool_player= {}
+      @sockets = {}
+    end
+
+    def find_by_socket(socket)
+      @sockets[socket.__id__]
+    end
+
+    def find_by_room_id(room_id)
+      @pool[room_id]
+    end
+
+    def find_by_player_id(player_id)
+      @pool_player[player_id]
+    end
+
+    def add(room_id, player_id, socket)
+      obj = {socket: socket, room_id: room_id, player_id: player_id}
+      @pool[room_id] ||= {}
+      @pool[room_id][player_id] = obj
+      @pool_player[player_id] = obj
+      @sockets[socket.__id__] = obj
+      self
+    end
+
+    def remove(socket)
+      remove_ find_by_socket(socket)
+    end
+
+    def remove_by_player_id(player_id)
+      remove_ find_by_player_id(player_id)
+    end
+
+    private
+
+    def remove_(obj)
+      return nil unless obj
+      @sockets.delete(obj[:socket].__id__)
+      @pool[obj[:room_id]].delete(obj[:player_id])
+      @pool_player.delete(obj[:player_id])
+      self
     end
   end
 end
