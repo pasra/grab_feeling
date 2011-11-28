e26c5ecd539f2f9a6f4c47f6b1151c83cd011eb4
Shota Fukumori (sora_h) <sorah@tubusu.net>
2011-11-28 17:56:51 +0900

    add Round#ends_at

--- lib/grab_feeling/model/round.rb
+++ lib/grab_feeling/model/round.rb
@@ -5,4 +5,8 @@ class Round < ActiveRecord::Base
   belongs_to :room
   belongs_to :theme
   belongs_to :drawer, :class_name => "Player", :foreign_key => "drawer_id"
+
+  def ends_at
+    self.started_at + Config["operation"]["turn"]
+  end
 end
