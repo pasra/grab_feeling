43080e80f0c3a0980697016e7dc22c4ee7af83ed
Shota Fukumori (sora_h) <sorah@tubusu.net>
2011-11-28 12:39:31 +0900

    fix timing after load conifg

--- lib/grab_feeling/config.rb
+++ lib/grab_feeling/config.rb
@@ -14,8 +14,19 @@ module GrabFeeling
       def yaml(force_reload=false)
         if force_reload || !@@yaml
           @@yaml = YAML.load_file(File.exist?(CONFIG_FILE) ? CONFIG_FILE : SAMPLE_CONFIG)
+          after_load
+          @@yaml
         else
           @@yaml
+        end
+      end
+
+      def after_load
+        if Config["theme_opening"] && Config["theme_opening"]["timing"]
+          timings = Config["theme_opening"]["timing"].to_a
+          Config["theme_opening"]["timing"] = timings.each_with_index.map do |t,i|
+            [t[0] - (i-1 > 0 ? timings[i-1][0] : 0), t[1]]
+          end
         end
       end
     end
